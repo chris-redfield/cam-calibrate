a414f31e95583ebbc74c9dcc0c702eb991382e80
chris <christianmoryah@gmail.com>
2021-03-03 10:09:18 -0300

    melhora comentarios no codigo, fix do vetor de translação

--- camcalibrate.py
+++ camcalibrate.py
@@ -48,11 +48,18 @@ while(True):
 
 print("Calibrando...", end='\n\n')
 
+# Realiza a calibração de câmera pelo OpenCV
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
+# Cria uma matriz zerada para incluir os dados dos vetores de rotação
 rotation_mat = np.zeros(shape=(3, 3))
+
+# Preenche a matriz com os vetores de rotação devolvidos pelo OpenCV
 R = cv2.Rodrigues(rvecs[0], rotation_mat)[0]
-P = np.column_stack((np.matmul(mtx,R), tvecs[0]))
+
+# Computa a matriz P, multiplicando a matriz de intrínsecos K pela 
+# matriz de rotação, e o vetor de translação
+P = np.column_stack( ( np.matmul(mtx,R), tvecs[0] ) )
 
 print("Matriz de projeção P:")
 print(P, end='\n\n')
@@ -62,11 +69,12 @@ print(mtx, end='\n\n')
 
 # print("Vetores de Rotação:")
 # print(rvecs[:2])
-print("Matriz de Rotação:")
+print("Matriz de rotação:")
 print(R, end='\n\n')
 
 print("Vetor de translação:")
-print(np.array(tvecs).flatten(), end='\n\n')
+#print(np.array(tvecs).flatten(), end='\n\n')
+print(tvecs[0], end='\n\n')
 
 print("Parâmetros de distorção: [k1, k2, p1, p2, k3]")
 print(dist, end='\n\n')
